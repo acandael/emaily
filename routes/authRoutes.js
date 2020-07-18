06bf9fd505472384021deb9d52b8875ab907e590
acandael <info@anthonycandaele.com>
2020-07-18 11:52:00 +0200

    fix issue with scope

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,9 +8,13 @@ module.exports = (app) => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'), {
-    scope: ['profile', 'email'],
-  });
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
