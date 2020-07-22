13dabfbb3c8b9107c7b935d0afc33ea8a37f39b1
acandael <info@anthonycandaele.com>
2020-07-22 11:46:10 +0200

    stuck at lesson 91, redirect uri mismatch

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,16 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          // We allready have a record with the given profile id
-          done(null, existingUser);
-        } else {
-          // We don't have a user record with this id, make a new record!
-          new User({ googleId: profile.id })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        // We allready have a record with the given profile id
+        return done(null, existingUser);
+      }
+      // We don't have a user record with this id, make a new record!
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
